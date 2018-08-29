939140757e6b5aa9b08f97242bad45fca2a1b58e
Thaís Nicoly Araújo <thais.toscano@ccc.ufcg.edu.br>
2018-08-29 16:41:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req, res) => res.send('Got a POST request'))
-app.put('/user', (req, res) => res.send('Got a PUT request at /user'))
-app.delete('/user', (req, res) => res.send('Got a DELETE request at /user'))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/', (req, res) => res.send('Receiving request for POST'));
+app.put('/user', (req, res) => res.send('Receiving request for PUT at /user'));
+app.delete('/user', (req, res) => res.send('Receiving request for DELETE at /user'));
 
-app.listen(3000, () => console.log('App running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'));
