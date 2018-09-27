856235cabc059c361794ceb321d2d00cb5da7a8d
thaisnat <thais.toscano@ccc.ufcg.edu.br>
2018-09-27 03:44:42 -0300

    passport configuration

--- passport.js
+++ passport.js
@@ -1,42 +1,71 @@
 const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
 const user = require("./router/user/user");
 
-module.exports = function (passport){
-    user.use(passport.initialize());
-    user.use(passport.session());
+module.exports = function (passport) {
 
-    user.get('/', (req, res) => { console.log('Inside the homepage callback')
-         console.log(req.sessionID)
-        res.send(`You got home page!\n`)
-    });
-
-    user.use(new LocalStrategy(
-    function(email, password, done) {
-        user.findOne({ email: email }, function (err, email) {
-        if (err) { return done(err); }
-        if (!email) { return done(null, false); }
-        if (!user.verifyPassword(password)) { return done(null, false); }
-        return done(null, user);
+    function findUser(username, callback) {
+        User.findOne({ "username": username }, function (err, doc) {
+            callback(err, doc);
         });
     }
-    ));
+
+    function findUserById(id, callback) {
+        const ObjectId = require("mongoose").ObjectId;
+        User.findById(id, (err, doc) => {
+            callback(err, doc);
+        });
+    }
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+
+    passport.use(new LocalStrategy(
+
+        function (email, password, done) {
+            user.findOne({ username: username }, function (err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false); }
+
+                if (!user.verifyPassword(password)) {
+                    return done(null, false, { message: 'Incorrect username. Please try again !' })
+                };
+                if (!user.validPassword(password)) {
+                    return done(null, false, { message: 'Incorrect password. Please try again !' });
+                }
+                return done(null, user);
+            });
+        }));
 
     user.post('/login', (req, res, next) => {
-    console.log('Inside POST /login callback');
-    passport.authenticate('local',
-    {succesuseredirect: '/', failureRedirect: 'login'});
-    req.login(user, (err)=> {
-    console.log('Inside req.login() callback');
-    return res.send('You were authenticated & logged in!\n');
-    })
-    (req, res, next);
+        console.log('Inside POST /login callback');
+        passport.authenticate('local',
+            { succesuseredirect: '/', failureRedirect: 'login' });
+        req.login(user, (err) => {
+            console.log('Inside req.login() callback');
+            return res.send('You were authenticated & logged in!\n');
+        })
+            (req, res, next);
     });
 
     user.get('/login', (req, res) => {
-    if(req.isAuthenticated()) {
-        res.send('you hit the authentication endpoint\n')
-    } else {
-        res.redirect('/')
-    }
+        if (req.isAuthenticated()) {
+            res.send('you hit the authentication endpoint\n')
+        } else {
+            res.redirect('/')
+        }
     })
+
+    user.get('/', (req, res) => {
+        console.log('Inside the homepage callback')
+        console.log(req.sessionID)
+        res.send(`You got home page!\n`)
+    });
 }
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const sr = express();
 const user = require("./router/user/user");
 const user = require("./router/question/question");
 const cache = require('memory-cache');
 const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-sr.get('/', (req, res) =>
-  res.send("Welcome to website")
-);
+const sr = express();
+const sr_port = process.env.PORT || 3000;
+
 
 // Body-Parser
 sr.use(bodyParser.urlencoded({ extended: false }));
@@ -42,32 +42,36 @@ cache.get('user');
 sr.use(cors());
 
 //conf cors
-sr.get('/login', cors(), function(req,res,next){
-  res.json({msg: "user login"});
+sr.get('/login', cors(), function (req, res, next) {
+  res.json({ msg: "user login" });
 })
 
-sr.get('/user/:id/question', cors(), function(req,res,next){
- res.json({msg: "user question by id"});
+sr.get('/user/:id/question', cors(), function (req, res, next) {
+  res.json({ msg: "user question by id" });
 })
 
 //session
 
-require ('./back/passport') (passport);
+require('./back/passport')(passport);
 
-sr.use (session ({
+sr.use(session({
 
-  //store: novo MongoStore ({
-  //  mongooseConnection: mongoose.connection,
-  //  ttl: 30 * 60 // = 30 minutos de sessão
-  //}),
-  //segredo: 'tempsecret',
- // resave: false,
- // saveUninitialized: false
-})
-
-);
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 30 * 60
+  }),
+  secret: 'tempsecret',
+  resave: false,
+  saveUninitialized: false
+}));
 
 sr.use(passport.initialize());
 sr.use(passport.session());
+
+sr.get('/', (req, res) =>
+  res.send("Welcome to website")
+);
+
+app.listen(app_port, () => console.log("The online academic monitoring system is ready and set on port " + sr_port + "!"))
 
 module.exports = sr;
